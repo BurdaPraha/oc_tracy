01c77b0e2e3d3e13c30e9f36a061044385892052
Michal Landsman <landsman@insuit.cz>
2017-05-31 14:12:58 +0200

    renamed panel

--- src/TracyPanel/Panel/TemplatesPanel.php
+++ src/TracyPanel/Panel/TemplatesPanel.php
@@ -7,7 +7,7 @@ use Tracy\IBarPanel;
 class TemplatesPanel implements IBarPanel
 {
     /** @var string */
-    private $title = "Templates suggestion";
+    private $title = "Templates";
 
     /** @var string */
     private $icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAANbY1E9YMgAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAADWSURBVDjLlZNNCsIwEEZzKW/jyoVbD+Aip/AGgmvRldCKNxDBv4LSfSG7kBZix37BQGiapA48ZpjMvIZAGRExwDmnESw7MMvsHnMFTdOQUsqjrmtXsggKEEVReCDseZc/HbOgoCxLDytwUEFBVVUe/fjNDguEEFGSAiml4Xq+DdZJAV78sM1oOpnT/fI0oEYPZ0lBtjuaBWSttcHtRQWvx9sMrlcb7+HQwxlmojfI9ycziGyj34sK3AV8zd7KFSYFCCwO1aMFsQgK8DO1bRsFM0HBP9i9L2ONMKHNZV7xAAAAAElFTkSuQmCC";
@@ -26,7 +26,7 @@ class TemplatesPanel implements IBarPanel
      */
     public function getTab()
     {
-        return "<span title=''><img src='{$this->icon}'> {$this->title}</span>";
+        return "<span title=''><img src='{$this->icon}'> {$this->title} (" . sizeof($this->getLog()) . ")</span>";
     }
 
     /**
